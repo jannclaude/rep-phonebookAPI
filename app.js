b3807aa4da6d65eefa0b2882f85f7abefcd69ddc
Jann Claude De Jesus <claudejann05@gmail.com>
2021-07-18 00:57:15 +0800

    update files

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+var jwt = require('jsonwebtoken');
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,11 +10,6 @@ const dotenv = require('dotenv');
 
 app.get('/', (req, res) => {
     res.send("Hello World!");
-});
-
-
-app.get('/friends', (req, res) =>{
-    res.send('Hello friends');
 });
 
 const connectDB = require('./config/db');
@@ -24,5 +21,54 @@ connectDB();
 
 // Routes
 app.use('/', require('./routes/index'));
+
+//model api
+var schema = new mongoose.Schema(
+    {
+        email: "string",
+        password: "string"
+    }
+);
+
+var User = mongoose.model('User', schema);
+
+//signup route api
+app.post("/signup", async (req, res) => {
+    const { email, password } = req.body;
+    console.log(email);
+    let user = await User.findOne({ email });
+
+    if (user) {
+        return res.json({ msg: "Email already taken" });
+    }
+
+    user = new User({
+        email,
+        password,
+    });
+
+    await user.save();
+    var token = jwt.sign({ id: user.id }, "password");
+    res.json({ token: token });
+});
+
+//login route api
+app.post("/login", async (req, res) =>{
+    const { email, password } = req.body;
+    console.log(email);
+
+    let user = await User.findOne({ email });
+    console.log(user);
+
+    if(!user) {
+        return res.json({ msg: "no user found with that email" });
+    }
+    if(user.password !== password) {
+        return res.json({ msg: "password is not correct" });
+    }
+
+    var token = jwt.sign({ id: user.id }, "password");
+    return res.json({ token: token });
+});
 
 app.listen(process.env.PORT || 3000);
