abd558471988adf3f285dec6cf6bc37e24c4b09b
jannclaude <jannclaudejesus@gmail.com>
2023-11-05 03:35:42 +0800

    update repo

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
+
 const connectDB = async () =>{
     try{
-        const conn = await mongoose.connect("mongodb+srv://claude08:claude08@mongoapi.nmwrm.mongodb.net/api-mongo", {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false
